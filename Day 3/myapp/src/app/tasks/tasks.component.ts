bcb935f983847ea90c192406b29d906737da3a11
monilSoni <monilsoni99@gmail.com>
2017-06-22 07:58:30 +0530

    Practice Arena

--- Day 3/myapp/src/app/tasks/tasks.component.ts
+++ Day 3/myapp/src/app/tasks/tasks.component.ts
@@ -6,14 +6,25 @@ import { Task } from "./task";
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+
+  Id:string='';
+  Title:string='';
+  Status:string='';
+
 allTasks:Task[]=[
   new Task('1','add to staging area','done'),
   new Task('2','commit to git','pending'),
   new Task('3','push to github','pending')
-
-]
+];
   constructor() { }
 
+  onDelete(item){
+    this.allTasks.splice(this.allTasks.indexOf(item), 1);
+  }
+
+  onAddTask(){
+    this.allTasks.push(new Task(this.Id, this.Title, this.Status));
+  }
   ngOnInit() {
   }
 
